4dbfeb361f07fb19781fbcbb55afa8e0bb73dd7a
steve thompson <steven.thompson.ca@gmail.com>
2017-06-18 17:51:47 -0400

    Updates Singleton.

--- Singleton.playground/Contents.swift
+++ Singleton.playground/Contents.swift
@@ -1,32 +1,32 @@
 import UIKit
 /*:
-# The Singleton Design Pattern:
-![](sing_def.png)
-- Singleton is an instance, and there is only 1 of them ever created.
-- The class will prevent any object from creating more than 1 instance of the class.
-- Also, this single instance is globally accessible. What does this mean?
-- It should also be lazy initialized. Why?
-- Why would we need a Singleton?
-*/
+ # The Singleton Design Pattern:
+ ![](sing_def.png)
+ - Singleton is an instance, and there is only 1 of them ever created.
+ - The class will prevent any object from creating more than 1 instance of the class.
+ - Also, this single instance is globally accessible. What does this mean?
+ - It should also be lazy initialized. Why?
+ - Why would we need a Singleton?
+ */
 
 class MyNetworkManagerSingleton {
 }
 
 let mnws = MyNetworkManagerSingleton()
 
-/*: 
-- Why is `mnws` not a Singleton?
-*/
 /*:
-- First step is we want to create a private initializer.
-- This is legal and it prevents outside classes from initializing this class.
-- But how do we initialize it then?
-*/
+ - Why is `mnws` not a Singleton?
+ */
+/*:
+ - First step is we want to create a private initializer.
+ - This is legal and it prevents outside classes from initializing this class.
+ - But how do we initialize it then?
+ */
 class MyNetworkManagerSingleton2 {
   private init() {}
 }
 
-//
+// Causes a compiler error
 // let mnws2 = MyNetworkManagerSingleton2()
 
 class MyNetworkManagerSingleton3 {
@@ -35,15 +35,15 @@ class MyNetworkManagerSingleton3 {
 }
 
 /*:
-- The trick is we want this class to initialize itself and retain itself.
-*/
+ - The trick is we want this class to initialize itself, and then retain itself.
+ */
 
 // let mnws3 = MyNetworkManagerSingleton3()
 
 /*:
-- To call the sharedInstance property we can't call it on an instance because we can't create one from the outside.
-- So, we have to make the property `static`.
-*/
+ - To call the `sharedInstance` property we can't call it on an *instance* because our init is now private.
+ - So, we have to make `sharedInstance` `static`.
+ */
 
 class MyBigSingleTon {
   private init() {
@@ -54,12 +54,17 @@ class MyBigSingleTon {
 
 
 var s1: MyBigSingleTon? = MyBigSingleTon.sharedInstance
+print(Unmanaged<AnyObject>.passUnretained(s1 as AnyObject).toOpaque())
+
 var s2: MyBigSingleTon? = MyBigSingleTon.sharedInstance
+print(Unmanaged<AnyObject>.passUnretained(s2 as AnyObject).toOpaque())
 
 s1 = nil
 s2 = nil
 
 let s3 = MyBigSingleTon.sharedInstance
+print(Unmanaged<AnyObject>.passUnretained(s3 as AnyObject).toOpaque())
+
 
 
 
