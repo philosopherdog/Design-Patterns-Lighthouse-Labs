49b0218933f2d9436208d934bb8a5161fe9db23b
steve thompson <steven.thompson.ca@gmail.com>
2017-06-18 17:51:22 -0400

    Updates general discussion of Design Patterns.

--- Design Patterns.playground/Contents.swift
+++ Design Patterns.playground/Contents.swift
@@ -14,20 +14,27 @@ import UIKit
  ## What are design patterns?
  - Design Patterns are general solutions to common problems in software development.
  - Many problems in software development are related to how flexible and resilient a program is to change over the long run.
- - Ideally we want to write software that can easily have features added/removed  without unduly involving code that shouldn't be impacted by these changes?
+ - Ideally we want to write software that can easily have features added or removed without those changes involving code that shouldn't be impacted by these changes?
  - For instance, by structuring our app properly we should be able to easily swap out `Core Data` for `Realm` without having to rebuild most of our application in the process?
- - Design patterns are generalized guidelines that apply regardless of the specific domain.
+ - But if we put all of our model logic in our view controllers, then if we wanted to change the persistence technology we would have to start making changes to code that shouldn't be impacted by this change.
+ - Design patterns are generalized guidelines for building robust code that applies regardless of the specific domain.
  - So they are the same in, say, game development as they are in an e-commerce app.
- - Design Patterns are general solutions on how best to structure your objects.
- - The area of design patterns trace their origins to the book Design Patterns: Elements of Reusable Object-Oriented Software by a group of 4 authors referred to as The Gang of Four.
+ - Design Patterns are general solutions on how best to structure your objects and their interactions with other objects.
+ - The area of design patterns trace their origins to the book "Design Patterns: Elements of Reusable Object-Oriented Software" by a group of 4 authors referred to as The Gang of Four.
  
 ![](gang.jpeg)
 
- - This book talks about 23 patterns. These have been extended since.
+ - This book talks about 23 patterns. 
+ - The original 23 patterns have since been extended.
  - Design patterns give you a shared vocabulary.
  - Design patterns give you ideas about how to build good software because you can see how some of the best developers have solved problems.
  - Using design patterns is reusing the experience of other developers.
  - I'm going to cover some classical design patterns today many of the examples are taken from the Head First Book: `Design Patterns`.
+ - Like any tool, design patterns can be abused. 
+ - The trade off with developing code to patterns is that your code can become more complex to understand. 
+ - But before you can know when to use design patterns you need to understand the main patterns.
+ - Today functional styles of programming are fashionable. Knowing OOP will make you a better programmer whether you're working in a functional paradigm or not. 
+ - Anyhow, although Swift has some functional elements it is still fundamentally and OO language, as is Objc.
  
 ![](head.jpeg)
 */
