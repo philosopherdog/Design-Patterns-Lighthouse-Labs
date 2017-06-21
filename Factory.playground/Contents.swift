4eea20fe42a857202614ae55cd943d15fa518301
steve thompson <steven.thompson.ca@gmail.com>
2017-06-20 22:01:13 -0400

    Updates factory

--- Factory.playground/Contents.swift
+++ Factory.playground/Contents.swift
@@ -8,12 +8,18 @@ import UIKit
  - Before we start on the Simple Factory Pattern we need to establish a few principles of OOP.
  - In this first example MasterVC is programming to implementations not to an interface.
  - This means that MasterVC has a dependency on the concrete types DetailVC1 and DetailVC2.
+ - This is not what we want!
  
  ### Open/Closed Principle
  - The consequence of programming to concrete objects rather than abstractions is that if we add more view controllers we have to open up MasterVC to add new switch cases.
  - This violates what is called the "Open/Closed Principle", which states that
  
  > "software entities (classes, modules, functions, etc.) should be open for extension, but closed for modification"; [Wikipedia](https://en.wikipedia.org/wiki/Open/closed_principle)
+ 
+ ### Polymorphism
+ - The other consequence of programming to concrete objects is that we can't take advantage of Polymorphism.
+ > Polymorphism allows you to use the same interface for different underlying concrete objects.
+ > A good analogy for polymorphism is a button which you can tap or click. All buttons share the same interface (click/tap). But buttons do different things depending on the situation/app. If buttons were not polymorphic they would be next to useless.
  */
 
 class DetailVC1: UIViewController {
@@ -42,16 +48,16 @@ class MasterVC: UIViewController {
 
 
 /*:
- - Right now MasterVC is dependent on 2 concrete view controller subclasses. This makes MasterVC fragile in the face of change.
- - Instead we can make MasterVC dependent on an abstraction or interface.
+ - Right now MasterVC is dependent on 2 concrete view controller subclasses DetailVC1 & 2. This makes MasterVC fragile in the face of change.
+ - Instead let's make MasterVC dependent on an abstraction or interface so that we can practice the principle of programming to an interface rather than an implementation.
  - By doing this our MasterVC will obey the `Open/Closed Principle`.
- - By programming to an interface we are able to use polymorphism.
+ - Our MasterVC will talk to it's dependent children polymorphically. It doesn't need to know any concrete details.
  - That is, setting `object` on the destination view controller can do different things for each concrete view controller, but the setter is the same regardless of implementation details. MasterVC does not need to know about the details.
  */
 
 // I make a protocol and create a new abstract type
 protocol MyData {
-  // this is a computed property
+  // this is a computed property.
   var object: AnyObject?{set get}
 }
 
@@ -92,7 +98,7 @@ class MasterVC2: UIViewController {
 
 /*:
  - We can now easily add new view controllers without having to alter anything in MasterVC.
- - MasterVC now obeys the Open/Closed Principle and programs to an interface not an implementation.
+ - MasterVC now programs to an interface not an implementation, obeys the Open/Closed Principle, and uses polymorphism to talk to its children! Pretty cool.
  */
 
 /*
@@ -114,8 +120,7 @@ class MasterVC2: UIViewController {
 
 /*:
  ## Simple Factory Pattern
- - Let's now look at the `Simple Factory Pattern`.
- - Demonstrating the problem that Simple Factory is trying to solve.
+ - Let's start by demonstrating the problem that Simple Factory is trying to solve.
  */
 
 
@@ -181,12 +186,14 @@ store.veg?.prepare()
 
 
 /*:
- ## Problems:
- - PizzaStore doesn't obey the Open/Closed Principle.
- - Also, code like this usually needs to create switch cases in most of its methods.
+ ## Some Problems With This Code:
+ - PizzaStore is dependent on concrete Pizza types.
+ - So, PizzaStore doesn't obey the Open/Closed Principle.
+ - Our concrete Pizza types share the same public methods, but this may not be the case. There's nothing enforcing that future Pizza types can handle specific message types.
+ - Also, code like this usually needs to create switch cases in many other methods.
  - If we need to add new Pizza types or remove Pizza types the PizzaStore class will have to be changed every place we have a conditional!
  - We can start to have an explosion of conditional code very easily.
- - Conditional code like this is hard to understand, hard extend, hard to maintain.
+ - Conditional code like this is hard to understand, hard extend, hard to maintain. It's a mess.
  - First of all let's fix some of the worst code repetition in our Pizza types by creating an interface/protocol. This way PizzaStore will not be as dependent on concrete Pizza types.
  */
 
@@ -254,10 +261,10 @@ store2.orderedPizza?.prepare()
 
 
 /*:
- - This code is an improvement over our first iteration, but the problem is that the PizzaStore still does not obey the Open/Closed Principle.
+ - This code is an improvement over our first version, but the problem is that the PizzaStore still does not obey the Open/Closed Principle.
  - This is because the PizzaStore needs to have knowledge of concrete Pizza types.
  - Whenever the concrete pizza types change we will have to open this code.
- - The HeadFirst discusses a principle that can help us here:
+ - HeadFirst discusses a principle that can help us here:
  > **Encapsulate What Varies:** [Software Engineering](https://softwareengineering.stackexchange.com/questions/337413/what-does-it-mean-when-one-says-encapsulate-what-varies)
  > Firstly, what does it mean to "encapsule" something, or what is "encapsulation"? [Wikipedia](https://en.wikipedia.org/wiki/Encapsulation_(computer_programming))
  > It refers to 2 related but distinct ideas:
@@ -267,8 +274,8 @@ store2.orderedPizza?.prepare()
  
  - We want to concentrate on the second definition of "encapsulate".
  - We can solve our problem by moving the creation code, which can and will change, outside the PizzaStore class and into its own class.
- - This is sometimes called a "Simple Factory" class.
- - It's only job is to create concrete Pizza objects and return them as generic Pizza objects.
+ - This solution is sometimes called a "Simple Factory" pattern.
+ - It's only job is to create concrete Pizza objects and return them upcasted to an abstract Pizza type.
  
  ![](fact.png)
  */
@@ -297,8 +304,8 @@ class PizzaStore3 {
   internal func orderPizza(of type: String) {
     orderedPizza = _factory.createPizza(of: type)
   }
-  internal func cost(for pizza: Pizza) -> Int {
-    return pizza.cost()
+  internal func cost() -> Int? {
+    return orderedPizza?.cost()
   }
 }
 
@@ -307,13 +314,14 @@ let type = "veggie"
 store3.orderPizza(of: type)
 store3.orderedPizza?.prepare()
 store3.orderedPizza?.cook()
+store3.cost()
 
 /*:
  - Notice PizzaStore3 now obeys the Open/Closed Principle.
  - We can easily add or remove Pizza types without impacting the PizzaStore.
  - The goal of this pattern is to separate the creation of the objects from the client (PizzaStore) that uses those objects.
  - This way changes to the creation code will leave the client unaffected.
- - By injecting the factory into the PizzaStore initializer we could pass in a dummy factory for unit testing. We could also create other factories and change them at run time if needed, but to do so we may want to expose the factory property.
+ - By "injecting" the factory into the PizzaStore initializer we could pass in a dummy factory for unit testing. We could also create other factories and change them at run time if needed, but to do so we may want to expose the factory property. What else would we need to do?
  - Passing the Factory into the PizzaStore's initializer or assigning it to the factory property from outside is called "Dependency Injection".
  - Dependency injection contrasts with initializing the factory internally, like, inside the initializer.
  - An analogy of Dependency Injection might be something like the difference between growing your own vegetables and having vegetables delivered to your door. What's the difference?
@@ -323,8 +331,9 @@ store3.orderedPizza?.cook()
 
 /*:
  ### Summary:
- - Open/Closed
+ - Open/Closed Principle.
  - Programming to an interface rather than to concrete details.
+ - Using Polymorphism.
  - Encapsulate what changes. That is, move what changes into its own class so that you limit the disruption that change can cause.
  */
 
@@ -344,45 +353,57 @@ store3.orderedPizza?.cook()
  > "Define an interface for creating an object, but let subclasses decide which class to instantiate. The Factory method lets a class defer instantiation it uses to subclasses." (Gang Of Four)
  
  - Most examples of the Factory Pattern that you find online confuse the Simple Factory for other types of factories.
- - The Factory Method Pattern is a distinct pattern, but it is related to the Simple Factory.
+ - The Factory Method Pattern is a distinct pattern, and it is easily confused with the Simple Factory.
  - The main difference is that the client doesn't have a reference to a concrete factory. Rather it has a reference to an abstract factory instead.
  
   ![](method.png)
  
- - This allows us to make run time decisions as to which factory to instantiate and use polymorphism to get different behavior just like we did with the Pizza objects in the Simple Factory but at the level of factories themselves.
+ - This allows us to make run time decisions as to which factory to instantiate.
+ - Let's consider a couple of different ways of doing this.
  
 
  */
 
-// Product
-class HomeInternetService {
+// Product (I could have used a class)
+protocol HomeInternetService {
+  var speed: Int {get}
+  func cost()-> Double
+}
+
+// Protocol extensions allow us to provide a default implementation
+extension HomeInternetService {
   var speed: Int {
     return 30
-  }
-  func cost()-> Double {
-    fatalError()
-  }
+    }
 }
 
 class BabyService: HomeInternetService {
-  override func cost()-> Double {
+  func cost()-> Double {
     return 40
   }
 }
 
 class PowerUserService: HomeInternetService {
-  override var speed: Int {
+  var speed: Int {
     return 60
   }
-  override func cost()-> Double {
+  func cost()-> Double {
     return 55
   }
 }
 
+// Factories
 class ISPFactory {
   var service: HomeInternetService
-  init(service: HomeInternetService) {
-    self.service = service
+  init(serviceType: String) {
+    switch serviceType {
+    case "Baby":
+      self.service = BabyService()
+    case "Power":
+      self.service = PowerUserService()
+    default:
+      self.service = BabyService()
+    }
   }
   func cost()-> Double {
     return service.cost()
@@ -401,17 +422,17 @@ class MontrealFactory: ISPFactory {
 }
 
 class OntarioFactory: ISPFactory {
+  // it gets the default cost for all packages
 }
 
-let powerUser = PowerUserService() as HomeInternetService
-let montrealFactory = MontrealFactory(service: powerUser) as ISPFactory
+let montrealFactory =  MontrealFactory(serviceType: "Power") as ISPFactory
 montrealFactory.cost()
-powerUser.speed
+montrealFactory.service.speed
 
 
-let ontarioFactory = OntarioFactory(service: powerUser) as ISPFactory
+let ontarioFactory = OntarioFactory(serviceType: "Power") as ISPFactory
 ontarioFactory.cost()
-powerUser.speed
+ontarioFactory.service.speed
 
 /*:
 
@@ -420,7 +441,7 @@ powerUser.speed
 ![](magic.png)
 
 - This is taken from Wikipedia.
-- The difference is that the concrete factory determines which concrete product is created.
+- The difference is that the concrete factory determines which concrete product is created. Moreover the particular factory determines the particular products that are created.
 */
 
 // abstract product
@@ -442,38 +463,31 @@ class Room: CustomStringConvertible {
 // concrete products
 class OrdinaryRoom: Room {
   override var description: String {
-    return super.description + " " + "the ordinary room"
+    return super.description + " " + "Ordinary"
   }
 }
 
 class MagicRoom: Room {
   override var description: String {
-    return super.description + " " + "the magic room"
+    return super.description + " " + "Magical"
   }
 }
 
 // Abstract factory
 class MazeGame {
   var gold: Int?
-  var rooms: [Room] = []
-  var room1: Room! {
-    didSet {
-      rooms.append(room1)
-    }
-  }
-  var room2: Room! {
-    didSet {
-      rooms.append(room2)
-    }
-  }
+  var rooms: [Room]!
+  var room1: Room!
+  var room2: Room!
   init(room1: Room, room2: Room) {
     room1.connect(to: room2)
     self.room1 = room1
     self.room2 = room2
+    rooms = [room1, room2]
   }
 }
 
-// concrete factories
+// concrete factories that handle distinct types of rooms
 class MagicMazeGame: MazeGame {
   init(room1: MagicRoom, room2: MagicRoom, gold: Int) {
     super.init(room1: room1, room2: room2)
